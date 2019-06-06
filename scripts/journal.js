e5997a48eecddd98b6019a322d7475c213b062b9
scottsilver <scottsilver47@gmail.com>
2019-06-06 17:23:17 -0500

    added entry about the definition of functions

--- scripts/journal.js
+++ scripts/journal.js
@@ -1,9 +1,17 @@
 const objectsJournalEntry = {
     date: "6/6/2019",
-    topic: "Dot notation vs bracket notation are ways to access values inside of an object. With dot notation we can access properties of an object by name with 'object.name'. You can do the same with bracket notation, but with different syntax: object['name']. Bracket notation can include variables and strings that start with numbers as long as they are written as a string within the brackets.",
-    entry: "Dot notation and bracket notation ",
+    topic: "Dot notation and bracket notation ",
+    entry: "Dot notation vs bracket notation are ways to access values inside of an object. With dot notation we can access properties of an object by name with 'object.name'. You can do the same with bracket notation, but with different syntax: object['name']. Bracket notation can include variables and strings that start with numbers as long as they are written as a string within the brackets.",
     mood: "anxious"
 }
 const entries = []
 entries.push(objectsJournalEntry)
 
+const objectsJournalEntry2 = {
+    date: "6/6/2019",
+    topic: "Functions and logic",
+    entry: "'Conjunction junction, what's your function'? For whatever reason, I think of this opening line to the school house rock song that explains the function of the grammar construct, conjunctions. And I think they are related in a very loose way. Conjunctions in English allow 'users' to string together thoughts without having to make a new sentence. While this can be verbose, 'and' and 'but' are kind of like functions you can call on in language to make your sentence a compound sentence. Functions allow you to execute a line of code whenever you need it. The contrast is that functions in JavaScript allow you to be less verbose. When the same lines of code need to be repeated, I can put them in a function instead of repeating myself.",
+    mood: "anxious"
+}
+const entries = []
+entries.push(objectsJournalEntry)
