7bf31f6f03ee74ad6623f392935c40931040d0ad
scottsilver <scottsilver47@gmail.com>
2019-06-06 17:12:52 -0500

    added entry for dot notation vs bracket notation

---
+++ scripts/journal.js
@@ -0,0 +1,9 @@
+const objectsJournalEntry = {
+    date: "6/6/2019",
+    topic: "Dot notation vs bracket notation are ways to access values inside of an object. With dot notation we can access properties of an object by name with 'object.name'. You can do the same with bracket notation, but with different syntax: object['name']. Bracket notation can include variables and strings that start with numbers as long as they are written as a string within the brackets.",
+    entry: "Dot notation and bracket notation ",
+    mood: "anxious"
+}
+const entries = []
+entries.push(objectsJournalEntry)
+
