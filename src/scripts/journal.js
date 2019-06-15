8182891aedd26209c3fe638d8cb4abbb38e76537
scottsilver <scottsilver47@gmail.com>
2019-06-15 17:47:46 -0500

    who knows

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -18,7 +18,6 @@ const renderJournalEntries = (entries) => {
     entryContainer.innerHTML += makeJournalEntryComponent(entries[i])
 }
 }
-
 fetch("http://localhost:3000/entries") // Fetch from the API
     .then( entries => entries.json())  // Parse as JSON
     .then(parsedEntries => {
