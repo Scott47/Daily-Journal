94980fb0316e252f4c0dc2ec46acd6d2251beada
scottsilver <scottsilver47@gmail.com>
2019-06-15 17:53:31 -0500

    checked to make sure Part 4 is working

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -18,7 +18,7 @@ const renderJournalEntries = (entries) => {
     entryContainer.innerHTML += makeJournalEntryComponent(entries[i])
 }
 }
-fetch("http://localhost:3000/entries") // Fetch from the API
+fetch("http://localhost:8088/entries") // Fetch from the API
     .then( entries => entries.json())  // Parse as JSON
     .then(parsedEntries => {
         renderJournalEntries(parsedEntries)
