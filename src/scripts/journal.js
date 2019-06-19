e21497ffc69306ffa158c6e3e0fd1edd0ae7785c
scottsilver <scottsilver47@gmail.com>
2019-06-18 19:33:11 -0500

    added event listener for record journal entry button

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -3,6 +3,18 @@ API.getJournalEntries ()
         renderJournalEntries(parsedEntries)
     })
 
+document.querySelector("#record-entry").addEventListener("click", (event) => {
+    let recordDate = document.querySelector("#journalDate").value 
+    let recordConcepts = document.querySelector("#concepts").value 
+    let recordContainer = document.querySelector("#journalEntry").value
+   console.log(recordDate, recordConcepts, recordContainer)
+})
+   
+   
+    // let newEntry = buildingJournalEntry(recordDate, recordConcepts, recordContainer)
+    // addNewEntry(newEntry)
+    // console.log(event.target)
+// })
     /*
     Main application logic that uses the functions and objects
     defined in the other JavaScript files.
