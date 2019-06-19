f533dad1fa7645da90cfb0132a73852ce289bc44
scottsilver <scottsilver47@gmail.com>
2019-06-18 20:56:21 -0500

    added conditional statement for required input validation

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -3,18 +3,35 @@ API.getJournalEntries ()
         renderJournalEntries(parsedEntries)
     })
 
-document.querySelector("#record-entry").addEventListener("click", (event) => {
+document.querySelector("#record-entry").addEventListener("click", () => {
+    event.preventDefault()
     let recordDate = document.querySelector("#journalDate").value 
     let recordConcepts = document.querySelector("#concepts").value 
-    let recordContainer = document.querySelector("#journalEntry").value
-   console.log(recordDate, recordConcepts, recordContainer)
+    let recordEntry = document.querySelector("#journalEntry").value
+    let recordMood = document.querySelector("#mood").value
+    if (recordDate === "" || recordConcepts === "" || recordEntry === "") {
+    } else 
+    {let saveEntry = newJournalEntry(recordDate, recordConcepts, recordEntry, recordMood)
+    API.saveJournalEntry(saveEntry)
+    }
+    })
+    
+const newJournalEntry = (date, concept, entry, mood) => ({
+    "Date": date,
+    "Concept": concept,
+    "Entry": entry,
+    "Mood": mood
 })
+
+    // Use `fetch` with the POST method to add your entry to your API
+    // fetch("http://localhost:8088/entries", { // Replace "url" with your API's URL
+    //     method: "POST",
+    //     headers: {
+    //         "Content-Type": "application/json"
+    //     },
+    //     body: JSON.stringify(newJournalEntry)
+    // })
    
-   
-    // let newEntry = buildingJournalEntry(recordDate, recordConcepts, recordContainer)
-    // addNewEntry(newEntry)
-    // console.log(event.target)
-// })
     /*
     Main application logic that uses the functions and objects
     defined in the other JavaScript files.
